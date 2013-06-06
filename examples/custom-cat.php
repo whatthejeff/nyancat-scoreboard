5b13dbccb3e6173c4cd3a022b8790c2b9080f82a
Jeff Welch <whatthejeff@gmail.com>
2013-06-05 22:15:29 -0400

    Added some examples.

---
+++ examples/custom-cat.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use NyanCat\Cat;
+use NyanCat\Rainbow;
+use NyanCat\Team;
+use NyanCat\Scoreboard;
+
+use Fab\Factory as FabFactory;
+
+$scoreboard = new Scoreboard(
+    new Cat(
+        array(
+            array(
+                '              ___  ',
+                '             / %_) ',
+                '     _.----./ /    ',
+                '    /        /     ',
+                ' __/ (  | (  |     ',
+                "/__.-'|_|--|_|     "
+            ),
+            array(
+                '               ___ ',
+                '              / %_)',
+                '     _.----._/ /   ',
+                '    /         /    ',
+                ' __/ (  | (  |     ',
+                "/__.-|_|--|_|      "
+            ),
+            array(
+                '              ___  ',
+                '             / %_) ',
+                '    _.----._/ /    ',
+                '   /         /     ',
+                ' _/ (  / /  /      ',
+                "/_/-|_/--|_/       "
+            )
+        )
+    ),
+    new Rainbow(
+        FabFactory::getFab(
+            empty($_SERVER['TERM']) ? 'unknown' : $_SERVER['TERM']
+        )
+    ),
+    array(
+        new Team('pass', 'green', '^'),
+        new Team('fail', 'red', 'o'),
+        new Team('pending', 'cyan', '-'),
+    )
+);
+
+$scoreboard->start();
+for($i = 0; $i < 100; $i++) {
+    usleep(90000);
+    $scoreboard->score('pass');
+}
+$scoreboard->end();
---
+++ examples/custom-rainbow.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use NyanCat\Cat;
+use NyanCat\Rainbow;
+use NyanCat\Team;
+use NyanCat\Scoreboard;
+
+use Fab\Factory as FabFactory;
+
+$scoreboard = new Scoreboard(
+    new Cat(),
+    new Rainbow(
+        FabFactory::getFab(
+            empty($_SERVER['TERM']) ? 'unknown' : $_SERVER['TERM']
+        ), 
+        array('+','-'), 64, 10
+    ),
+    array(
+        new Team('pass', 'green', '^'),
+        new Team('fail', 'red', 'o'),
+        new Team('pending', 'cyan', '-'),
+    )
+);
+
+$scoreboard->start();
+for($i = 0; $i < 100; $i++) {
+    usleep(90000);
+    $scoreboard->score('pass');
+}
+$scoreboard->end();
